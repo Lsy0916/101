c9b407e381e87a191fc0608cdd64887b07e60ec0
L <1522311390@qq.com>
2025-12-22 10:02:33 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Microsoft YaHei", Arial, sans-serif;
+}
+
+#app {
+  padding-top: 70px;
+}
+</style>
